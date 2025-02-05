25ac9d70693ab6c429fca3e5d78a0c87dc8afb32
tjomemmanuel <tjomemmanuel@gmail.com>
2025-02-05 12:21:48 -0500

    Initialisation du projet et de quelques routes vers la creations des modules

---
+++ server.js
@@ -0,0 +1,70 @@
+// Aller chercher les configurations de l'application
+import 'dotenv/config';
+
+// Importer les fichiers et librairies
+import express, { json, urlencoded } from 'express';
+import expressHandlebars from 'express-handlebars';
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
+import cspOption from './csp-options.js'
+
+// Création du serveur
+const app = express();
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
+
+// Ajout de middlewares
+app.use(helmet(cspOption));
+app.use(compression());
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+// Ajouter les routes ici ...
+app.get('/', (request, response) => {
+    response.render('home', {
+        title: 'Page d\'accueil',
+    });
+});
+
+
+/** Creation de mes route pour modules */
+
+// Route vers le module bar de Navigation
+app.get('/moduleheader', async (request, response) => {
+    response.render("modules/module-header", {
+        titre: "ReadEasy | Modules bar de navigation",
+       
+      });
+});
+
+
+// Route vers le module footer
+app.get('/modulefooter', async (request, response) => {
+    response.render("modules/module-footer", {
+        titre: "ReadEasy | Modules pied de page",
+      });
+});
+
+// Route vers le module footer
+app.get('/modulecaroussel', async (request, response) => {
+    response.render("modules/module-caroussel", {
+        titre: "ReadEasy | Modules caroussel image",
+      });
+});
+
+
+
+
+// Renvoyer une erreur 404 pour les routes non définies
+app.use(function (request, response) {
+    // Renvoyer simplement une chaîne de caractère indiquant que la page n'existe pas
+    response.status(404).send(request.originalUrl + ' not found.');
+});
+
+// Démarrage du serveur
+app.listen(process.env.PORT);
+console.info(`Serveurs démarré:`);
+console.info(`http://localhost:${ process.env.PORT }`);
