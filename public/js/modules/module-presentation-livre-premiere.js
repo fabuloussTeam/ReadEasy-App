045e22bee66513acd3a45a664b8e400a9f42a2e5
boubad02 <boubasiddy02@gmail.com>
2025-02-22 22:55:15 -0500

    Modification positions image et liens

--- public/js/modules/module-presentation-livre-premiere.js
+++ public/js/modules/module-presentation-livre-premiere.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const image = document.querySelector(".image-section img");
+  const image = document.querySelector(".image-section img");
 
-    image.style.transition = "transform 0.2s ease-out"; 
+  image.style.transition = "transform 0.2s ease-out";
 
-    image.addEventListener("mouseover", function () {
-        image.style.transform = "scale(1.04)"; 
-        image.style.transformOrigin = "center"; 
-    });
+  image.addEventListener("mouseover", function () {
+    image.style.transform = "scale(1.1)";
+    image.style.transformOrigin = "center";
+  });
 
-    image.addEventListener("mouseout", function () {
-        image.style.transform = "scale(1)";
-    });
+  image.addEventListener("mouseout", function () {
+    image.style.transform = "scale(1)";
+  });
 });
