b2f9cb37d6806d70ea3a3fda5323802f5a461044
tjomemmanuel <tjomemmanuel@gmail.com>
2025-03-27 14:13:41 -0400

    Mise a jours des package vulnerable

--- server.js
+++ server.js
@@ -148,15 +148,8 @@ app.get('/', async (request, response) => {
     try {
         const meslivresalaune = await getlivres();
         const randomBooks = getRandomBooks(meslivresalaune, 4);
-       // console.log(`Random books: ${randomBooks.map(book => book.titre).join(', ')}`);
 
         let itOption = false;
-
-       // verification des session
-       //   console.log(`request reauest user: ${JSON.stringify(request.user)}`);
-       //  console.log(`request session:  ${ JSON.stringify(request.session)}`);
-       //  console.log(`request session user:  ${ JSON.stringify(request.session.passport)}`);
-
        if (request.user && (request.session.passport.user == request.user.id_utilisateur)) {
           // console.log(`request user: ${JSON.stringify(request.user)}`);
             itOption = true;
@@ -355,40 +348,7 @@ app.patch("/api/livre/:id_livre", upload.fields([{ name: 'url_image' }, { name: 
     }
 });
 
-// ======================================================================
-/*
-// Route pour ajouter un livre
-app.post("/api/livre", async (request, response) => {
-   
-    try {
-        const { 
-            isbn,
-            titre,
-            description,
-            prix,
-            est_gratuit,
-            auteur,
-            url_image
-          } = request.body;
-         console.log("c'est mange:"+ isbn, titre, description, prix, est_gratuit, auteur, url_image);
-          
-        const livre = await addlivre(
-            isbn, 
-            titre, 
-            description, 
-            prix, 
-            est_gratuit, 
-            auteur,
-            url_image
-        );
-        return response
-            .status(200)
-            .json({ livre, message: "Livre ajoutée avec succès" });
-    } catch (error) {
-        return response.status(400).json({ error: error.message });
-    }
-});
-*/
+
 // Route pour obtenir la liste des livres
 app.get("/api/livres", async (request, response) => {
     try {
@@ -567,121 +527,6 @@ app.delete("/api/panier/:id_panier", async (request, response) => {
         return response.status(400).json({ error: error.message });
     }
 });
-
-
-
-
-
-
-/*********** Creation des modules reuitilisables *********/
-
-// Route vers le module bar de Navigation
-app.get('/moduleheader', async (request, response) => {
-    response.render("modules/module-header", {
-        titre: "ReadEasy | Modules bar de navigation",
-        styles: ["/css/modules/module-header.css"],
-        scripts: ["/js/modules/module-header.js"],
-      });
-});
-
-// Route vers le module footer
-app.get('/modulefooter', async (request, response) => {
-    response.render("modules/module-footer", {
-        titre: "ReadEasy | Modules pied de page",
-        styles: ["/css/modules/module-footer.css"],
-        scripts: ["/js/modules/module-footer.js"],
-      });
-});
-
-
-app.get('/modulecaroussel', async (request, response) => {
-
-    var imgs = [
-        { url_image: "https://user-images.githubusercontent.com/78242022/273443252-b034e050-3d70-48ef-9f0f-2d77ef9b2604.jpg", alt: "Image 01" },
-        { url_image: "https://user-images.githubusercontent.com/78242022/273443252-b034e050-3d70-48ef-9f0f-2d77ef9b2604.jpg", alt: "Image 02" },
-        { url_image: "https://user-images.githubusercontent.com/78242022/273443248-130249b5-87b7-423d-9281-48d810bcd30d.jpg", alt: "Image 03" }
-    ]
-
-    response.render("modules/module-caroussel", {
-        titre: "ReadEasy | Modules caroussel image",
-        styles: ["/css/modules/module-caroussel.css"],
-        scripts: ["/js/modules/module-caroussel.js"],
-        images: imgs
-      });
-});
-
-// Route vers le module presentation livre style 01 
-app.get('/modulepresentationstyleun', async (request, response) => {
-    response.render("modules/module-presentation-livre-premiere", {
-        titre: "ReadEasy | Modules presentation d'un livre style un",
-        styles: ["/css/modules/module-presentation-livre-premiere.css"],
-        scripts: ["/js/modules/module-presentation-livre-premiere.js"],
-      });
-});
-
-// Route vers le module presentation livre style 02
-app.get('/modulepresentationstyledeux', async (request, response) => {
-    response.render("modules/module-presentation-livre-deuxieme", {
-        titre: "ReadEasy | Modules presentation d'un livre style deux",
-        styles: ["/css/modules/module-presentation-livre-deuxieme.css"],
-        scripts: ["/js/modules/module-presentation-livre-deuxieme.js"],
-      });
-});
-
-// Route vers module un livre a la une
-app.get('/modulelivrealaune', async (request, response) => {
-    response.render("modules/module-livre-a-laune", {
-        titre: "ReadEasy | Modules presentation d'un livre a la une",
-        styles: ["/css/modules/module-livre-a-laune.css"],
-        scripts: ["/js/modules/module-livre-a-laune.js"],
-      });
-});
-
-// Route vers module espace publicitaire
-app.get('/moduleespacepublicitaire', async (request, response) => {
-    response.render("modules/module-espace-publicitaire", {
-        titre: "ReadEasy | Modules espace publicitaire",
-        styles: ["/css/modules/module-espace-publicitaire.css"],
-        scripts: ["/js/modules/module-espace-publicitaire.js"],
-      });
-});  
-
-// Route vers module nos partenaires
-app.get('/modulenospartenaires', async (request, response) => {
-    response.render("modules/module-nos-partenaires", {
-        titre: "ReadEasy | Modules nos partenaires",
-        styles: ["/css/modules/module-nos-partenaires.css"],
-        scripts: ["/js/modules/module-nos-partenaires.js"],
-      });
-});
-
-// Route vers le module connexion utilisateur
-app.get('/moduleconnexion', async (request, response) => {
-    response.render("modules/module-connexion", {
-        titre: "ReadEasy | Module connexion utilisateur",
-        styles: ["/css/modules/module-connexion.css"],
-        scripts: ["/js/modules/module-connexion.js"],
-      });
-});
-
-// Route vers le module creation de compte utilisateur
-app.get('/modulecreationdecompte', async (request, response) => {
-    response.render("modules/module-creationdecompte", {
-        titre: "ReadEasy | Module creation de compte ",
-        styles: ["/css/modules/module-creationdecompte.css"],
-        scripts: ["/js/modules/module-creationdecompte.js"],
-    });
-});
-
-// Route vers le module creation de compte utilisateur
-/** app.get('/panierachats', async (request, response) => {
-    response.render("modules/module-panier-achats", {
-        titre: "ReadEasy | Module panier d'achat ",
-        styles: ["/css/modules/module-panier-achats.css"],
-        scripts: ["/js/modules/module-panier-achats.js"],
-    });
-}); */
-
 
 
 // Renvoyer une erreur 404 pour les routes non définies
